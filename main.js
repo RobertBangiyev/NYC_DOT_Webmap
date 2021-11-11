f6386555a4edeb1c6a1349ad79ed9b396aa56763
Robert Bangiyev <rbangiyev123@gmail.com>
2021-11-10 21:54:46 -0500

    Geoserver capability update

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-// const { ol } = require("./libs/v6.9.0-dist/ol");
-
-// const { ol } = require("./libs/v6.9.0-dist/ol");
-
 window.onload = init;
 
 function init() {
     const map = new ol.Map({
         view: new ol.View({
-            // extent: [four coordinates to set extent to nyc],
             // projection: 'EPSG:2263' <-figure out how to make this work
             center: [-8236575.792110519, 4973235.140319245],
             zoom: 5,
-            // maxZoom: 14,
             minZoom: 4,
             extent: [-8267536.611922189, 4938222.188713483, -8200877.417120842, 4996755.287155063]
         }),
@@ -72,10 +66,7 @@ function init() {
           visible: false,
           title: 'USATopo'
     });
-    // map.addLayer(stamenTerrain);
-    
-      
-    //Layer Group
+
     const baseLayerGroup = new ol.layer.Group({
         layers: [openStreetMapStandard, openStreetMapHumanitarian, stamenTerrain, worldTopo, usaTopo]
     });
@@ -83,14 +74,12 @@ function init() {
 
     map.addLayer(baseLayerGroup);
 
-    //layer switcher logic for basemaps
-    // const baseLayerElements = document.querySelectorAll('input[type=radio]');
     const baseLayerElements = document.querySelectorAll('.basemap');
     const basedLayerElements = document.querySelectorAll('.baselayer');
 
     const randomlayer = new ol.layer.Image({
         title: "Wetlands",
-        visible: true,
+        visible: false,
         source: new ol.source.ImageWMS({
             url: "http://localhost:8080/geoserver/wms",
             params: {'LAYERS': 'nyc:nyc_wetlands'},
@@ -110,8 +99,6 @@ function init() {
         visible: false
     });
 
-    // nyclayer.setVisible();
-
     const cafelayer = new ol.layer.Image({
         title: "Sidewalk Cafe Regulations",
         visible: false,
@@ -122,8 +109,6 @@ function init() {
             serverType: 'geoserver'
         })
     });
-    // const nyclayer = new ol.layer.WMS("Wetlands", 
-    //     "http://localhost:8080/geoserver/wms/nyc", {layers: 'nyc:nyc_roads'});
 
     const basedLayerGroup = new ol.layer.Group({
         layers: [nyclayer, randomlayer, cafelayer]
@@ -154,7 +139,7 @@ function init() {
         })
     });
 
-    let whichCheckedLayers = 'Wetlands';
+    let whichCheckedLayers = '';
     basedLayerElements.forEach((basedLayerElement) => {
         basedLayerElement.addEventListener('change', () => {
             let basedLayerElementValue = basedLayerElement.value;
@@ -167,20 +152,6 @@ function init() {
             })
         })
     });
-
-    // map.on('moveend', (e) => {
-    //     baseLayerGroup.getLayers().forEach((element, index, array) => {
-    //         if(Math.round(map.getView().getZoom()) < 15) {
-    //             console.log(`element index: ${index} \ncurrentthing: ${Math.round(map.getView().getZoom()) - 11}`)
-    //             element.setVisible(index == Math.round(map.getView().getZoom()) - 11);
-    //         } else {
-    //             let baseLayerTitle = element.get('title');
-    //             element.setVisible(baseLayerTitle === whichChecked);
-    //             console.log(whichChecked);
-    //         }
-    //     })
-    //     console.log(Math.round(map.getView().getZoom()))
-    // })
 
     let currentZoom = map.getView().getZoom();
     map.on('moveend', (e) => {
@@ -199,16 +170,6 @@ function init() {
             console.log(Math.round(map.getView().getZoom()))
         }
     })
-    // console.log(baseLayerElements);
-
-
-    // let url = 'http://localhost:8080/geoserver/rest/workspaces/nyc/featuretypes.json';
-
-    // fetch(url)
-    // .then(res => res.json())
-    // .then(out =>
-    // console.log('Checkout this JSON! ', out))
-    // .catch(err => console.log(err));
 
     const addedlayers = ['Wetlands in NYC', 'Street and Highway Capital Reconstruction Projects', 'Sidewalk Cafe'];
 
@@ -219,7 +180,6 @@ function init() {
 
     addlayerbtn.addEventListener('click', function(e) {
         const parser = new ol.format.WMSCapabilities();
-
         fetch('http://localhost:8080/geoserver/wms?service=wms&version=1.1.1&request=GetCapabilities')
         .then(function (response) {
             return response.text();
@@ -227,7 +187,6 @@ function init() {
         .then(function (text) {
             const result = parser.read(text);
             const a = result['Capability']['Layer']['Layer']
-            // console.log(result['Capability']['Layer']['Layer'])
             for(let i = 0; i < a['length']; i++) {
                 if(!addedlayers.includes(a[i].Title)) {
                     let addthis = `<label for="` + a[i].Name + `">` + "\n" + `<input type="checkbox" ` + `class="newadds" id="` + a[i].Title + `" name="` + a[i].Name + `" value="` + a[i].Name + `">` + a[i].Title + "\n" + `</label><br>`;
@@ -237,8 +196,6 @@ function init() {
             }
             addlayerbtn.classList.add('hidden');
             addselectedbtn.classList.remove('hidden');
-            // console.log(a['length'])
-            // document.getElementById('log').innerText = JSON.stringify(result, null, 2);
         });
     });
 
@@ -272,16 +229,12 @@ function init() {
         });
     });
 
-
-    let lastChecked = "";
     function somefunction() {
         if(addedlayers.length != 0) {
-            // console.log("heerre")
             const addedLayerElements = document.querySelectorAll('.addedlayer');
             addedLayerElements.forEach((addedLayerElement) => {
                 addedLayerElement.addEventListener('change', () => {
                     let addedLayerElementValue = addedLayerElement.value;
-                    // console.log(addedLayerElementValue);
                     addedLayerGroup.getLayers().forEach((element, index, array) => {
                         console.log(element.get('title'));
                         let addedLayerTitle = element.get('title');
@@ -290,7 +243,5 @@ function init() {
                 })
             });
         }
-    }
-    
-    
+    } 
 };
