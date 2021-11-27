763aa4db151c585704f95936835d4df47ba88037
Robert Bangiyev <rbangiyev123@gmail.com>
2021-11-27 17:36:56 -0500

    Added basic measurments, layer feature display, ui improvements

--- main.js
+++ main.js
@@ -10,7 +10,15 @@
         }),
         target: 'js-map'
     })
-    map.on('click', (e) => console.log(e.coordinate));
+    const theMap = document.querySelector('#js-map');
+    map.on('click', getInfoBase);
+    map.addControl(new ol.control.ZoomSlider());
+
+    // let mousePosition = new ol.control.MousePosition();
+    // map.addControl(mousePosition);
+
+    let fullScreen = new ol.control.FullScreen({label:'F'});
+	map.addControl(fullScreen);
 
     //basemaps
     const openStreetMapStandard = new ol.layer.Tile({
@@ -71,7 +79,7 @@
     const basedLayerElements = document.querySelectorAll('.baselayer');
 
     const randomlayer = new ol.layer.Image({
-        title: "Wetlands",
+        title: "nyc_wetlands",
         visible: false,
         source: new ol.source.ImageWMS({
             url: "http://localhost:8080/geoserver/wms",
@@ -82,7 +90,7 @@
     });
     
     const nyclayer = new ol.layer.Image({
-        title: "Streets and Highways Reconstruction",
+        title: "street_and_highway",
         source: new ol.source.ImageWMS({
             url: "http://localhost:8080/geoserver/wms",
             params: {'LAYERS': 'nyc:street_and_highway'},
@@ -93,7 +101,7 @@
     });
 
     const cafelayer = new ol.layer.Image({
-        title: "Sidewalk Cafe Regulations",
+        title: "sidewalk_cafe",
         visible: false,
         source: new ol.source.ImageWMS({
             url: "http://localhost:8080/geoserver/wms",
@@ -109,6 +117,10 @@
 
     const addedLayerGroup = new ol.layer.Group({
         layer: []
+    });
+
+    const allLayerGroup = new ol.layer.Group({
+        layers: [nyclayer, randomlayer, cafelayer]
     });
 
     map.addLayer(basedLayerGroup);
@@ -130,11 +142,72 @@
         });
     }
 
+    let layerSwitcher = new ol.control.LayerSwitcher({
+        activationMode: 'click',
+        startActive: true,
+        tipLabel: 'Layers', // Optional label for button
+        groupSelectStyle: 'group', // Can be 'children' [default], 'group' or 'none'
+        collapseTipLabel: 'Collapse layers',
+      });
+
+    map.addControl(layerSwitcher);
+    
+    function getInfoBase(evt) {
+        document.getElementById('info').innerHTML = '';
+        const viewResolution = /** @type {number} */ (map.getView().getResolution());
+        let no_layers = allLayerGroup.getLayers().get('length');
+        let url = new Array();
+	    let wmsSource = new Array();
+	    let layer_title = new Array();
+        for (let i = 0; i < no_layers; i++) {
+            let visibility = allLayerGroup.getLayers().item(i).getVisible();
+            if(visibility == true) {
+                layer_title[i] = allLayerGroup.getLayers().item(i).get('title');
+                wmsSource[i] = new ol.source.ImageWMS({
+                    url: 'http://localhost:8080/geoserver/wms',
+                    params: {'LAYERS': layer_title[i]},
+                    serverType: 'geoserver',
+                    crossOrigin: 'anonymous'
+                });
+                url[i] = wmsSource[i].getFeatureInfoUrl(
+                    evt.coordinate, viewResolution, 'EPSG:3857',
+                    {'INFO_FORMAT': 'text/html'});
+                if (url[i]) {
+                    fetch(url[i])
+                      .then((response) => response.text())
+                      .then((html) => {
+                        // console.log(html);
+                        document.getElementById('info').innerHTML = html;
+                        const hasData = document.getElementsByClassName('featureInfo');
+                        if(hasData.length > 0) {
+                            theMap.classList.add('space-map');
+                            console.log("has something");
+                        }
+                        else {
+                            theMap.classList.remove('space-map');
+                        }
+                    });
+                }
+            }
+        }
+    }
+
     let whichChecked = 'OSMStandard';
     handleSelect(whichChecked, baseLayerElements, baseLayerGroup);
 
     let whichCheckedLayers = '';
-    handleSelect(whichCheckedLayers, basedLayerElements, basedLayerGroup);
+    basedLayerElements.forEach((basedLayerElement) => {
+        basedLayerElement.addEventListener('change', () => {
+            let basedLayerElementValue = basedLayerElement.value;
+            basedLayerGroup.getLayers().forEach((element, index, array) => {
+                let basedLayerTitle = element.get('title');
+                element.setVisible(basedLayerTitle === basedLayerElementValue);
+                if(basedLayerTitle === basedLayerElementValue) {
+                    whichCheckedLayers = basedLayerElementValue;
+                }
+            })
+        })
+    });
 
     let currentZoom = map.getView().getZoom();
     map.on('moveend', (e) => {
@@ -144,9 +217,9 @@
                 if(Math.round(map.getView().getZoom()) - 12 >= 0 && Math.round(map.getView().getZoom()) < 15) {
                     console.log(`element index: ${index} \ncurrentthing: ${Math.round(map.getView().getZoom()) - 12}`)
                     element.setVisible(index == Math.round(map.getView().getZoom()) - 12);
-                    console.log(element.get('title'));
                 } else {
                     let basedLayerTitle = element.get('title');
+                    console.log("checked: " + whichCheckedLayers);
                     element.setVisible(basedLayerTitle === whichCheckedLayers);
                 }
             })
@@ -194,11 +267,12 @@
 
     addselectedbtn.addEventListener('click', function(e) {
         const checks = document.querySelectorAll('.newadds');
+        console.log(checks);
         checks.forEach((e) => {
             if(e.checked) {
                 console.log(e.value);
                 const newlayer = new ol.layer.Image({
-                    title: e.id,
+                    title: e.name,
                     visible: false,
                     source: new ol.source.ImageWMS({
                         url: "http://localhost:8080/geoserver/wms",
@@ -208,9 +282,10 @@
                     })
                 });
                 addedLayerGroup.getLayers().insertAt(0, newlayer);
-                console.log(addedLayerGroup.getLayers());
+                allLayerGroup.getLayers().insertAt(0, newlayer);
+                // console.log(addedLayerGroup.getLayers());
                 addedlayers.push(e.id);
-                let addthis = `<input type="radio" class='addedlayer' name='addedLayerRadio' id='${e.id}' value='${e.id}'>${e.id}<br>`;
+                let addthis = `<input type="radio" class='addedlayer' name='addedLayerRadio' id='${e.name}' value='${e.id}'>${e.id}<br>`;
                 added.innerHTML += addthis;
                 if(layerschecked != "") {
                     document.getElementById(`${layerschecked}`).checked = true;
@@ -233,7 +308,8 @@
             const addedLayerElements = document.querySelectorAll('.addedlayer');
             addedLayerElements.forEach((addedLayerElement) => {
                 addedLayerElement.addEventListener('change', () => {
-                    let addedLayerElementValue = addedLayerElement.value;
+                    let addedLayerElementValue = addedLayerElement.id;
+                    console.log(addedLayerElementValue);
                     addedLayerGroup.getLayers().forEach((element, index, array) => {
                         let addedLayerTitle = element.get('title');
                         element.setVisible(addedLayerTitle === addedLayerElementValue);
@@ -355,4 +431,271 @@
         titlelayer.value = "";
         addfile.value = "";
     })
+
+    const raster = new ol.layer.Tile({
+        source: new ol.source.OSM(),
+      });
+      
+      const source = new ol.source.Vector();
+      
+      const vector = new ol.layer.Vector({
+        source: source,
+        style: new ol.style.Style({
+          fill: new ol.style.Fill({
+            color: 'rgba(255, 255, 255, 0.2)',
+          }),
+          stroke: new ol.style.Stroke({
+            color: '#ffcc33',
+            width: 2,
+          }),
+          image: new ol.style.Circle({
+            radius: 7,
+            fill: new ol.style.Fill({
+              color: '#ffcc33',
+            }),
+          }),
+        }),
+      });
+      
+      /**
+       * Currently drawn feature.
+       * @type {import("../src/ol/Feature.js").default}
+       */
+       let sketch;
+      
+       /**
+        * The help tooltip element.
+        * @type {HTMLElement}
+        */
+       let helpTooltipElement;
+       
+       /**
+        * Overlay to show the help messages.
+        * @type {Overlay}
+        */
+       let helpTooltip;
+       
+       /**
+        * The measure tooltip element.
+        * @type {HTMLElement}
+        */
+       let measureTooltipElement;
+       
+       /**
+        * Overlay to show the measurement.
+        * @type {Overlay}
+        */
+       let measureTooltip;
+       
+       /**
+        * Message to show when the user is drawing a polygon.
+        * @type {string}
+        */
+       const continuePolygonMsg = 'Click to continue drawing the polygon';
+       
+       /**
+        * Message to show when the user is drawing a line.
+        * @type {string}
+        */
+       const continueLineMsg = 'Click to continue drawing the line';
+      
+       /**
+       * Handle pointer move.
+       * @param {import("../src/ol/MapBrowserEvent").default} evt The event.
+       */
+      
+       let enableDraw = false;
+      const pointerMoveHandler = function (evt) {
+        if(enableDraw) {
+          if (evt.dragging) {
+            return;
+          }
+          /** @type {string} */
+          let helpMsg = 'Click to start drawing';
+      
+          if (sketch) {
+            const geom = sketch.getGeometry();
+            if (geom instanceof ol.geom.Polygon) {
+              helpMsg = continuePolygonMsg;
+            } else if (geom instanceof ol.geom.LineString) {
+              helpMsg = continueLineMsg;
+            }
+          }
+      
+          helpTooltipElement.innerHTML = helpMsg;
+          helpTooltip.setPosition(evt.coordinate);
+      
+          helpTooltipElement.classList.remove('hidden');
+        }
+      };
+      
+      map.addLayer(vector);
+
+      map.on('pointermove', pointerMoveHandler);
+      
+      map.getViewport().addEventListener('mouseout', function () {
+        if(enableDraw) {
+          helpTooltipElement.classList.add('hidden');
+        }
+      });
+      
+      const typeSelect = document.getElementById('type');
+      
+      let draw; // global so we can remove it later
+      
+      /**
+       * Format length output.
+       * @param {LineString} line The line.
+       * @return {string} The formatted length.
+       */
+      const formatLength = function (line) {
+        const length = ol.sphere.getLength(line);
+        let output;
+        if (length > 100) {
+          output = Math.round((length / 1000) * 100) / 100 + ' ' + 'km';
+        } else {
+          output = Math.round(length * 100) / 100 + ' ' + 'm';
+        }
+        return output;
+      };
+      
+      /**
+       * Format area output.
+       * @param {Polygon} polygon The polygon.
+       * @return {string} Formatted area.
+       */
+       const formatArea = function (polygon) {
+        const area = ol.sphere.getArea(polygon);
+        let output;
+        if (area > 10000) {
+          output = Math.round((area / 1000000) * 100) / 100 + ' ' + 'km<sup>2</sup>';
+        } else {
+          output = Math.round(area * 100) / 100 + ' ' + 'm<sup>2</sup>';
+        }
+        return output;
+      };
+      
+      function addInteraction() {
+        const type = typeSelect.value == 'area' ? 'Polygon' : typeSelect.value == "length" ? 'LineString' : "None";
+        if(type == 'None') {
+          if(enableDraw == true) {
+            helpTooltipElement.innerHTML = "";
+            helpTooltipElement.classList.add('hidden');
+            helpTooltipElement.parentNode.removeChild(helpTooltipElement);
+          }
+          enableDraw = false;
+          map.removeInteraction(draw);
+        }
+        else {
+          draw = new ol.interaction.Draw({
+            source: source,
+            type: type,
+            style: new ol.style.Style({
+              fill: new ol.style.Fill({
+                color: 'rgba(255, 255, 255, 0.2)',
+              }),
+              stroke: new ol.style.Stroke({
+                color: 'rgba(0, 0, 0, 0.5)',
+                lineDash: [10, 10],
+                width: 2,
+              }),
+              image: new ol.style.Circle({
+                radius: 5,
+                stroke: new ol.style.Stroke({
+                  color: 'rgba(0, 0, 0, 0.7)',
+                }),
+                fill: new ol.style.Fill({
+                  color: 'rgba(255, 255, 255, 0.2)',
+                }),
+              }),
+            }),
+          });
+          map.addInteraction(draw);
+      
+          createMeasureTooltip();
+          createHelpTooltip();
+          enableDraw = true;
+      
+          let listener;
+          draw.on('drawstart', function (evt) {
+            // set sketch
+            sketch = evt.feature;
+      
+            /** @type {import("../src/ol/coordinate.js").Coordinate|undefined} */
+            let tooltipCoord = evt.coordinate;
+      
+            listener = sketch.getGeometry().on('change', function (evt) {
+              const geom = evt.target;
+              let output;
+              if (geom instanceof ol.geom.Polygon) {
+                output = formatArea(geom);
+                tooltipCoord = geom.getInteriorPoint().getCoordinates();
+              } else if (geom instanceof ol.geom.LineString) {
+                output = formatLength(geom);
+                tooltipCoord = geom.getLastCoordinate();
+              }
+              measureTooltipElement.innerHTML = output;
+              measureTooltip.setPosition(tooltipCoord);
+            });
+          });
+      
+          draw.on('drawend', function () {
+            measureTooltipElement.className = 'ol-tooltip ol-tooltip-static';
+            measureTooltip.setOffset([0, -7]);
+            // unset sketch
+            sketch = null;
+            // unset tooltip so that a new one can be created
+            measureTooltipElement = null;
+            createMeasureTooltip();
+            ol.Observable.unByKey(listener);
+          });
+        }
+      }
+      
+      /**
+       * Creates a new help tooltip
+       */
+       function createHelpTooltip() {
+        if (helpTooltipElement && enableDraw) {
+          console.log(enableDraw);
+          helpTooltipElement.parentNode.removeChild(helpTooltipElement);
+        }
+        helpTooltipElement = document.createElement('div');
+        helpTooltipElement.className = 'ol-tooltip hidden';
+        helpTooltip = new ol.Overlay({
+          element: helpTooltipElement,
+          offset: [15, 0],
+          positioning: 'center-left',
+        });
+        map.addOverlay(helpTooltip);
+      }
+      
+      /**
+       * Creates a new measure tooltip
+       */
+       function createMeasureTooltip() {
+        if (measureTooltipElement) {
+          measureTooltipElement.parentNode.removeChild(measureTooltipElement);
+        }
+        measureTooltipElement = document.createElement('div');
+        measureTooltipElement.className = 'ol-tooltip ol-tooltip-measure';
+        measureTooltip = new ol.Overlay({
+          element: measureTooltipElement,
+          offset: [0, -15],
+          positioning: 'bottom-center',
+          stopEvent: false,
+          insertFirst: false,
+        });
+        map.addOverlay(measureTooltip);
+      }
+      
+      /**
+       * Let user change the geometry type.
+       */
+      typeSelect.onchange = function () {
+        map.removeInteraction(draw);
+        addInteraction();
+      };
+      
+      addInteraction();
 // };
